d3babdb59e0530329b4a45cc24147b655207aa2e
deepak-vsn <techcloud17@vsndirect.com>
2025-01-19 12:47:12 +0530

    Update App_Service.tf

--- App_Service.tf
+++ App_Service.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service" "app-service-sws-uat-01" {
+/*resource "azurerm_app_service" "app-service-sws-uat-01" {
   name                = "app-service-sws-uat-01"
   resource_group_name = azurerm_resource_group.uat-rg.name
   location            = azurerm_resource_group.uat-rg.location
@@ -13,4 +13,4 @@ resource "azurerm_app_service" "app-service-sws-uat-01" {
 resource "azurerm_app_service_virtual_network_swift_connection" "app" {
   app_service_id = azurerm_app_service.app-service-sws-uat-01.id
   subnet_id      = azurerm_subnet.sn2-si.id
-}
+}*/
