9f96494ef725b504a9635c6ee8c4d62831e3ee14
deepak-vsn <techcloud17@vsndirect.com>
2025-01-19 12:45:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-provider "azurerm" {
+/*provider "azurerm" {
   features {}
 
   client_id       = var.client_id
@@ -193,4 +193,4 @@ resource "azurerm_log_analytics_workspace" "log-analytics-workspace" {
   location            = azurerm_resource_group.rg-common.location
   resource_group_name = azurerm_resource_group.rg-common.name
   retention_in_days   = 90
-}
+}*/
