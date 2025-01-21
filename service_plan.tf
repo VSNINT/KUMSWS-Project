8e34a3bfd7cc239bd928f8e2638b9e648992d329
vikas-vsn <techcloud15@vsndirect.com>
2025-01-21 12:29:00 +0530

    Update service_plan.tf

--- service_plan.tf
+++ service_plan.tf
@@ -1,11 +1,11 @@
-/*resource "azurerm_app_service_plan" "gokeodbsws-appservice-plan" {
-  name                = "app-service-plan-sws-uat-rg"
-  resource_group_name = azurerm_resource_group.uat-rg.name
-  location            = azurerm_resource_group.uat-rg.location
+resource "azurerm_app_service_plan" "gokeodbsws-appservice-plan" {
+  name                = "app-service-plan-sws-prod-rg"
+  resource_group_name = azurerm_resource_group.prod-rg.name
+  location            = azurerm_resource_group.prod-rg.location
   kind                = "Windows" # Can be "Windows" depending on your needs
   reserved            = false     
   sku {
     tier = "Standard"
     size = "S2"
   }
-}*/
+}
