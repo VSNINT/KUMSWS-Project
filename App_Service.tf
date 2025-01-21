c65fc485b93f0383cf264ae173b6ff8b1dbc81f3
vikas-vsn <techcloud15@vsndirect.com>
2025-01-21 12:27:01 +0530

    Update App_Service.tf

--- App_Service.tf
+++ App_Service.tf
@@ -1,7 +1,7 @@
-/*resource "azurerm_app_service" "app-service-sws-uat-01" {
-  name                = "app-service-sws-uat-01"
-  resource_group_name = azurerm_resource_group.uat-rg.name
-  location            = azurerm_resource_group.uat-rg.location
+resource "azurerm_app_service" "app-service-sws-prod-01" {
+  name                = "app-service-sws-prod-01"
+  resource_group_name = azurerm_resource_group.prod-rg.name
+  location            = azurerm_resource_group.prod-rg.location
   app_service_plan_id = azurerm_app_service_plan.gokeodbsws-appservice-plan.id
 
   site_config {
@@ -11,6 +11,6 @@
 }
  # App Service VNet Swift Connection
 resource "azurerm_app_service_virtual_network_swift_connection" "app" {
-  app_service_id = azurerm_app_service.app-service-sws-uat-01.id
+  app_service_id = azurerm_app_service.app-service-sws-prod-01.id
   subnet_id      = azurerm_subnet.sn2-si.id
-}*/
+}
