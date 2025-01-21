0278886ca35bab98153b423f74a7c4931e103aa0
vikas-vsn <techcloud15@vsndirect.com>
2025-01-21 12:27:54 +0530

    Update Storage_Account.tf

--- Storage_Account.tf
+++ Storage_Account.tf
@@ -1,24 +1,17 @@
-/*resource "azurerm_storage_account" "storageaccountuat01" {
-  name                     = "storageaccountuat01"
-  resource_group_name      = azurerm_resource_group.uat-rg.name
-  location                 = azurerm_resource_group.uat-rg.location
+resource "azurerm_storage_account" "storageaccountprod01" {
+  name                     = "storageaccountprod01"
+  resource_group_name      = azurerm_resource_group.prod-rg.name
+  location                 = azurerm_resource_group.prod-rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags = {
-    environment = "uat"
+    environment = "prod"
   }
 }
 
-resource "azurerm_storage_container" "container-uat-01" {
-  name                  = "container-uat-01"
-  storage_account_id    = azurerm_storage_account.storageaccountuat01.id
+resource "azurerm_storage_container" "container-prod-01" {
+  name                  = "container-prod-01"
+  storage_account_id    = azurerm_storage_account.storageaccountprod01.id
   container_access_type = "private"
 }
 
-resource "azurerm_storage_blob" "blob-uat-01" {
-  name                   = "blob-uat-01"
-  storage_account_name   = azurerm_storage_account.storageaccountuat01.name
-  storage_container_name = azurerm_storage_container.container-uat-01.name
-  type                   = "Block"
-  source                 = "content-file.zip"
-}*/
